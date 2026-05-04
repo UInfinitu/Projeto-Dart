ddbeac81c4fcf0a4406fd2600c9b5d90f1cded5a
AllanShima <allanshinhama@gmail.com>
2026-05-04 16:27:37 -0300

    autenticacao no router

--- lib/router.dart
+++ lib/router.dart
@@ -1,14 +1,33 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:projeto_integrador/providers/Servico_Autenticacao.dart';
 
 import 'features/autenticacao/presentation/screens/login_screen.dart';
 import 'features/autenticacao/presentation/screens/register_screen.dart';
 import 'features/homepage/presentation/screens/homepage_screen.dart';
 
+final servicoAuth = ServicoAutenticacao();
+
 final GoRouter router = GoRouter(
   initialLocation: '/',
+  redirect: (BuildContext context, GoRouterState state) {
+    final autenticado = servicoAuth.autenticado;
+    final naRotaDeLogin = state.matchedLocation == "./login";
+
+    if (!autenticado && !naRotaDeLogin) {
+      return '/login';
+    }
+
+    if (autenticado && naRotaDeLogin) {
+      return '/';
+    }
+
+    // Caso não necessite de redirecionamento
+    return null;
+  },
   routes: [
     GoRoute(
-      path: '/',
+      path: '/login',
       builder: (context, state) => const LoginScreen(),
     ),
     GoRoute(
@@ -16,7 +35,7 @@ final GoRouter router = GoRouter(
       builder: (context, state) => const RegisterScreen(),
     ),
     GoRoute(
-      path: '/homepage',
+      path: '/',
       builder: (context, state) => const HomepageScreen(),
     ),
   ],
